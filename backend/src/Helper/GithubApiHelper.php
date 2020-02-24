a590c88856120dd17a89c991b5c35fcb8960edc6
Serhii Ponidielnik <s.ponidelnik@gmail.com>
2020-02-24 10:15:29 +0200

    backend download 3. simple, visual fixes

--- backend/src/Helper/GithubApiHelper.php
+++ backend/src/Helper/GithubApiHelper.php
@@ -1,6 +1,10 @@
 <?php
 
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class GithubApiHelper
 {
@@ -9,22 +13,29 @@ class GithubApiHelper
      * For test task it's look's like we not needed something more difficult
      * @param string $username
      * @return array
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
      */
     public static function showUser(string $username): array
     {
         $client = HttpClient::create();
-        try {
-            $response = $client->request('GET', 'http://api.github.com/users/' . $username);
-            $statusCode = $response->getStatusCode();
-            if ($statusCode == 200) {
-                $content = $response->getContent();
-                return $content->toArray();
-            } else {
-                error_log('github connection error with status code:#' . $statusCode);
+
+        $response = $client->request('GET', 'http://api.github.com/users/' . $username);
+        $statusCode = $response->getStatusCode();
+        if ($statusCode == 200) {
+            $content = $response->getContent();
+            if (is_string($content)) {
+                return json_decode($content, TRUE);
             }
-        } catch (Throwable $throwable) {
-            error_log($throwable->getMessage());
+            if (is_object($content)) {//it's a strange logic of httpClient detect json
+                return $content->toArray();
+            }
+        } else {
+            error_log('github connection error with status code:#' . $statusCode);
         }
+
         return [];
     }
 }
